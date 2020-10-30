6c1c8f8585fa8ccaa39e3d44feb4fbfd1e45966a
Jorge Kean De los Reyes (BDA/Outsourcing/HJORGE308W <jorge.kean.de.los.reyes@towerswatson.com>
2020-10-30 12:28:00 +0800

    Updates

---
+++ precache-manifest.3d17b0d679d25b8b4ecf3c4ece90bbae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992789746bfe5e2e2dae0bf1c4845bd4",
+    "url": "/jorgekean/ACWebsiteTemplate1/index.html"
+  },
+  {
+    "revision": "bb1255e3814e0d1261da",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/css/main.f4af0bd1.chunk.css"
+  },
+  {
+    "revision": "33a42903524be2ac8492",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/js/2.5da6f47e.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/js/2.5da6f47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1255e3814e0d1261da",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/js/main.820d0eee.chunk.js"
+  },
+  {
+    "revision": "acb91afd79994a7c064a",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/js/runtime-main.e6b45202.js"
+  },
+  {
+    "revision": "5e116615312e9be1d705b45cc1c95bd2",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/media/CSE.5e116615.png"
+  },
+  {
+    "revision": "fa8a48b4188ba1fb751fe0516b4f0afc",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/media/TCS.fa8a48b4.png"
+  },
+  {
+    "revision": "ecb32f04d16961dd1d8b1fa76a2be255",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/media/WD.ecb32f04.png"
+  },
+  {
+    "revision": "b4acf84075c24ca093f3afae3d5a4a19",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/media/home-background.b4acf840.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jorgekean/ACWebsiteTemplate1/static/media/logo.5d5d9eef.svg"
+  }
+]);
